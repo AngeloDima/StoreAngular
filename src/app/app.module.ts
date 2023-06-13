3f4e449445c288a5fd038680ba8e0d815cf4b99c
AngeloDima <angelodimauro91@gmail.com>
2023-06-13 21:37:51 +0200

    passo al routing l'id di ogni prodotto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { VetrinaComponent } from './home/vetrina/vetrina.component'
+import { VetrinaComponent } from './home/vetrina/vetrina.component';
+import { DettailComponent } from './home/dettail/dettail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    VetrinaComponent
+    VetrinaComponent,
+    DettailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/dettail/dettail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dettail',
+  templateUrl: './dettail.component.html',
+  styleUrls: ['./dettail.component.css']
+})
+export class DettailComponent {
+
+}
