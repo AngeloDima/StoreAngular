cedbb694a146ab13264be71e2d361757b99c13a5
AngeloDima <angelodimauro91@gmail.com>
2023-06-21 23:20:03 +0200

    carico i prodotto della categoria donna

--- src/app/donna/donna.component.ts
+++ src/app/donna/donna.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllProducts } from '../models/all-products';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-donna',
   templateUrl: './donna.component.html',
   styleUrls: ['./donna.component.css']
 })
-export class DonnaComponent {
+export class DonnaComponent implements OnInit {
+
+  data: AllProducts[] = []
+
+  constructor(private dataServ: DataService) { }
+
+  ngOnInit(): void {
+    this.dataServ.getDonna().subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    });
+  }
 
 }
