a1867ddcc7d2dc8826b072c634bacef4f059c020
AngeloDima <angelodimauro91@gmail.com>
2023-06-22 15:29:44 +0200

    implemento la logic del cambiamento di stato NgOnChanges

--- src/app/home/categoria/categoria.component.ts
+++ src/app/home/categoria/categoria.component.ts
@@ -1,24 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ServLocaleService } from 'src/app/verifico/serv-locale.service';
-
 
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
   styleUrls: ['./categoria.component.css']
 })
-export class CategoriaComponent implements OnInit {
-
+export class CategoriaComponent implements OnChanges {
   @Input()
-  p?: any
-
+  p?: any; //dati di
   veri: boolean = false;
 
-  constructor(private logServ: ServLocaleService) { }
-
-  ngOnInit(): void {
+  constructor(private logServ: ServLocaleService) {
     this.veri = this.logServ.utenteValido;
     console.log("controllo", this.veri);
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['p'] && this.p !== undefined) {
+      this.veri = this.logServ.utenteValido;
+    }
+  }
+
 }
+
+
+
