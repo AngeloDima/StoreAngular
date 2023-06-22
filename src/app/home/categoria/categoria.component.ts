ff4d69d3cc5ecc7f67957155f4f5f01b8d9c8cf8
AngeloDima <angelodimauro91@gmail.com>
2023-06-22 14:56:30 +0200

    l'accesso funziona ma non cambiano le scritte

--- src/app/home/categoria/categoria.component.ts
+++ src/app/home/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ServLocaleService } from 'src/app/verifico/serv-locale.service';
 
 
 @Component({
@@ -6,8 +7,18 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './categoria.component.html',
   styleUrls: ['./categoria.component.css']
 })
-export class CategoriaComponent {
+export class CategoriaComponent implements OnInit {
 
   @Input()
   p?: any
+
+  veri: boolean = false;
+
+  constructor(private logServ: ServLocaleService) { }
+
+  ngOnInit(): void {
+    this.veri = this.logServ.utenteValido;
+    console.log("controllo", this.veri);
+  }
+
 }
--- src/app/verifico/serv-locale.service.ts
+++ src/app/verifico/serv-locale.service.ts
@@ -29,3 +29,6 @@ export class ServLocaleService {
     });
   }
 }
+
+
+
